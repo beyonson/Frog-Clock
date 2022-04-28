1349469531a2dd27f80e0d8f214d588864e54cde
Garrett Marcinak <77867663+beyonson@users.noreply.github.com>
2022-04-28 10:47:20 -0400

    Added working I2C code

---
+++ OLED/i2c_drive.c
@@ -0,0 +1,138 @@
+#include "i2c_drive.h"
+
+void i2c_init(char i2c, unsigned short speed_mode)
+{
+	// enable io clock
+	RCC->APB2ENR |= 1; 
+	
+	if(i2c==1)
+	{
+		// enable i2c1 clock
+		RCC->APB1ENR |= 0x200000; 
+		
+		// pin enable
+		init_GP(PB, 6, OUT50, O_AF_OD);
+		init_GP(PB, 7, OUT50, O_AF_OD);
+		I2C1->CR1 |= 0x8000;
+		I2C1->CR1 &= ~0x8000;
+		I2C1->CR2 = 0x8;
+		
+		// set i2c speed
+		I2C1->CCR = speed_mode;
+		I2C1->TRISE = 0x9;
+		
+		// enable register
+		I2C1->CR1 |= 1;
+	}	else {
+		// enable i2c1 clock
+		RCC->APB1ENR |= 0x400000; 
+		
+		// pin enable
+		init_GP(PB, 10, OUT50, O_AF_OD);
+		init_GP(PB, 11, OUT50, O_AF_OD);
+		I2C2->CR1 |= 0x8000;
+		I2C2->CR1 &= ~0x8000;
+		I2C2->CR2 = 0x8;
+		
+		// set i2c speed
+		I2C2->CCR = speed_mode;
+		I2C2->TRISE = 0x9;
+		
+		// enable register
+		I2C2->CR1 |= 1;
+	}
+}
+
+// sending the address and r/w
+void i2c_add(char i2c, char address, char RW)
+{
+	volatile  int tmp;
+	if (i2c==1) 
+	{
+		I2C1->DR = (address|RW);
+		while((I2C1->SR1 & 2)==0);
+		while(I2C1->SR1 & 2) 
+		{
+			tmp = I2C1->SR1;
+			tmp = I2C1->SR2;
+			if((I2C1->SR1)==0)
+			{break;}
+		} 
+	} else if (i2c==2) 
+	{
+		I2C2->DR = (address|RW);
+		while((I2C2->SR1 & 2)==0);
+		while(I2C2->SR1 & 2) 
+		{
+			tmp = I2C2->SR1;
+			tmp = I2C2->SR2;
+			if((I2C2->SR1)==0)
+			{break;}
+		} 
+	}
+}
+
+// start 
+void i2c_start(char i2c)
+{
+	if (i2c==1) 
+	{
+		I2C1->CR1 |= 0x100;  // setting up start bit
+		while (!(I2C1->SR1 & 1)){}
+	} 
+	else if (i2c==2)
+	{
+		I2C2->CR1 |= 0x100;  // setting up start bit
+		while (!(I2C2->SR1 & 1)){}
+	}
+}
+
+// sending data
+void i2c_data(char i2c, char data)
+{
+	if (i2c==1)
+	{
+		while((I2C1->SR1 & 0x80)==0);
+		I2C1->DR = data;
+		while((I2C1->SR1 & 0x80)==0);
+	}
+	else if (i2c==2)
+	{
+		while((I2C2->SR1 & 0x80)==0);
+		I2C2->DR = data;
+		while((I2C2->SR1 & 0x80)==0);
+	}
+}
+
+// stop
+void i2c_stop(char i2c)
+{
+	volatile int tmp;
+	if (i2c==1) 
+	{
+		tmp = I2C1->SR1;
+		tmp = I2C1->SR2;
+		I2C1->CR1 |= 0x200;
+	} 
+	else if (i2c==2)
+	{
+		tmp = I2C2->SR1;
+		tmp = I2C2->SR2;
+		I2C2->CR1 |= 0x200;
+	}
+}
+
+
+// write function
+void i2c_write(char i2c, char address, char data[])
+{
+	int i=0;
+	i2c_start(i2c);
+	i2c_add(i2c, address, 0);
+	while(data[i])
+	{	
+		i2c_data(i2c, data[i]);
+		i++;
+	}
+	i2c_stop(i2c);
+}
---
+++ OLED/oled_drive.c
@@ -0,0 +1,139 @@
+#include "oled_drive.h"
+
+void oled_cmd_1byte(char i2c, char data)
+{
+	i2c_start(i2c);
+	i2c_add(i2c, 0x78, 0); // choosing oled address
+	
+	i2c_data(i2c, 0x00); // send command
+	
+	i2c_data(i2c, data);
+	
+	i2c_stop(i2c);
+}
+
+void oled_cmd_2byte(char i2c, char data[])
+{
+	int i=0;
+	i2c_start(i2c);
+	i2c_add(i2c, 0x78, 0); // choosing oled address
+	
+	i2c_data(i2c, 0x00); // send command
+	for(i=0; i<2; i++)
+		{i2c_data(i2c, data[i]);}
+	
+	i2c_stop(i2c);
+}
+
+
+// oled initialization
+void oled_init(char i2c)
+{
+	i2c_init(i2c, i2c_FM);
+	
+	char cmd[] = {0xA8, 0x3F};
+	oled_cmd_2byte(i2c, cmd);
+	char cmd1[] = {0xD3, 0x00};
+	oled_cmd_2byte(i2c, cmd1);
+	
+	oled_cmd_1byte(i2c, 0x40);
+	oled_cmd_1byte(i2c, 0xA1);
+	oled_cmd_1byte(i2c, 0xC8);
+	
+	char cmd2[] = {0xDA, 0x12};
+	oled_cmd_2byte(i2c, cmd2);
+	char cmd3[] = {0x81, 0x7F};
+	oled_cmd_2byte(i2c, cmd3);
+	
+	oled_cmd_1byte(i2c, 0xA4);
+	oled_cmd_1byte(i2c, 0xA6);
+
+	char cmd4[] = {0xD5, 0x80};
+	oled_cmd_2byte(i2c, cmd4);
+	char cmd5[] = {0x8D, 0x14};
+	oled_cmd_2byte(i2c, cmd5);
+	
+	oled_cmd_1byte(i2c, 0xAF);
+	
+	char cmd6[] = {0x20, 0x10};
+	oled_cmd_2byte(i2c, cmd6);
+}
+
+
+// oled data
+void oled_data(char i2c, char data)
+{
+	i2c_start(i2c);
+	i2c_add(i2c, 0x78, 0); // choosing oled address
+	
+	i2c_data(i2c, 0x40); // send data
+	
+	i2c_data(i2c, data);
+	
+	i2c_stop(i2c);
+}
+
+// oled position
+void oled_pos(char i2c, char Ypos, char Xpos)
+{
+	oled_cmd_1byte(i2c, 0x00 + (0x0F & Xpos)); // x position
+	oled_cmd_1byte(i2c, 0x10 + (0x0F & (Xpos>>4)));
+
+	oled_cmd_1byte(i2c, 0xB0 + Ypos); // y position
+}
+
+// blank screen
+void oled_blank(char i2c)
+{
+	int i, j;
+	oled_pos(i2c, 0, 0);
+	for (i=0; i<8; i++)
+	{
+		for (j=0; j<128; j++)
+		{
+			oled_data(i2c, 0x00);
+		}
+	}
+	oled_pos(i2c, 0, 0);
+}
+
+// print
+void oled_print(char i2c, char str[])
+{
+	int i, j;
+	i = 0;
+	while(str[i])
+	{
+		for(j=0; j<5; j++)
+		{
+			oled_data(i2c, ASCII[(str[i]-32)][j]);
+		}
+		i++;
+	}
+}
+
+
+// message
+void oled_msg(char i2c, char Ypos, char Xpos, char str[])
+{
+	oled_pos(i2c, Ypos, Xpos);
+	
+	oled_print(i2c, str);
+}
+
+// ascii intput
+void oled_Aprint(char i2c, int asc)
+{
+	int j;
+	for(j=0; j<5; j++)
+	{
+		oled_data(i2c, ASCII[asc][j]);
+	}
+}
+
+// clock output
+void oled_clock(char i2c, int time)
+{
+	oled_Aprint(i2c, ((time/10)%10)+16);
+	oled_Aprint(i2c, (time%10)+16);
+}
